3231511d351dc53308c7587feb9f134d58227c61
اسمك على GitHub <     GitHub>
2025-03-25 14:18:35 -0700

    add ServicesRepository , ProviderServicesRepository , Payment 2

---
+++ Repositories/ProviderServicesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Dorak.Models;
+
+namespace Repositories
+{
+    public class ProviderServicesRepository  : BaseRepository<ProviderService>
+    {
+        public ProviderServicesRepository(DorakContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
