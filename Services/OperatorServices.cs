ab1934b376b105caee8e15c6abefd6eb88c9988f
Mahmoud <mahmoudabdou743@gmail.com>
2025-04-26 20:11:29 +0300

    new Task "Start shift by operator"

--- Services/OperatorServices.cs
+++ Services/OperatorServices.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 using Data;
 using Dorak.Models;
 using Dorak.Models;
+using Dorak.Models.Models.Wallet;
 using Dorak.ViewModels;
 using Microsoft.AspNetCore.Identity;
+using Models.Enums;
 using Repositories;
 
 namespace Services
@@ -17,15 +19,16 @@ namespace Services
         public OperatorRepository operatorRepository;
         public ClientRepository clientRepository;
         private readonly AppointmentRepository appointmentRepository;
+        private readonly ShiftRepository shiftRepository;
         public CommitData commitData;
-        public OperatorServices(OperatorRepository _operatorRepository, CommitData _commitData, AppointmentRepository _appointmentRepository, ClientRepository _clientRepository)
+        public OperatorServices(OperatorRepository _operatorRepository, CommitData _commitData, AppointmentRepository _appointmentRepository, ClientRepository _clientRepository, ShiftRepository _shiftRepository)
         {
+            shiftRepository = _shiftRepository;
             operatorRepository = _operatorRepository;
             clientRepository = _clientRepository;
             appointmentRepository = _appointmentRepository;
             commitData = _commitData;
         }
-
         public async Task<IdentityResult> CreateOperator(string userId, OperatorViewModel model)
         {
             var _operator = new Operator
@@ -40,30 +43,6 @@ namespace Services
             operatorRepository.Add(_operator);
             commitData.SaveChanges();
             return IdentityResult.Success;
-        }
-
-        public Appointment CreateAppointment(AppointmentViewModel model)
-        {
-            var appointment = new Appointment
-            {
-                AppointmentDate = model.AppointmentDate,
-                AppointmentStatus = model.AppointmentStatus,
-                ClientType = model.ClientType,
-                Fees = model.Fees,
-                AdditionalFees = model.AdditionalFees,
-                EstimatedTime = model.EstimatedTime,
-                ExactTime = model.ExactTime,
-                EndTime = model.EndTime,
-                OperatorId = model.OperatorId,
-                
-                ShiftId = model.ShiftId,
-                UserId = model.UserId,
-                TemporaryClientId = model.TemporaryClientId,
-                CreatedAt = DateTime.UtcNow,
-                UpdatedAt = DateTime.UtcNow
-            };
-
-            return appointmentRepository.CreateAppoinment(appointment);
         }
         public bool SoftDelete(string operatorId)
         {
@@ -102,12 +81,59 @@ namespace Services
         }
         public IQueryable<Operator> GetAllOperators()
         {
-            var operators = operatorRepository.GetAll().Where(t=>t.IsDeleted!=true);
-            if(operators != null)
+            var operators = operatorRepository.GetAll().Where(t => t.IsDeleted != true);
+            if (operators != null)
             {
                 return operators;
             }
             return null;
+        }
+        public Appointment CreateAppointment(AppointmentViewModel model)
+        {
+            var appointment = new Appointment
+            {
+                AppointmentDate = model.AppointmentDate,
+                AppointmentStatus = model.AppointmentStatus,
+                ClientType = model.ClientType,
+                Fees = model.Fees,
+                AdditionalFees = model.AdditionalFees,
+                EstimatedTime = model.EstimatedTime,
+                ExactTime = model.ExactTime,
+                EndTime = model.EndTime,
+                OperatorId = model.OperatorId,
+
+                ShiftId = model.ShiftId,
+                UserId = model.UserId,
+                TemporaryClientId = model.TemporaryClientId,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            return appointmentRepository.CreateAppoinment(appointment);
+        }     //NOT DONE>>>>>>>>
+        public bool StartShift(int ShiftId, string operatorId)
+        {
+            DateTime currentTime = DateTime.Now;
+            TimeOnly timeNow = TimeOnly.FromDateTime(currentTime);
+
+            Shift shift = shiftRepository.GetById(s => s.ShiftId == ShiftId);
+
+            if (shift != null)
+            {
+                shift.OperatorId = operatorId;
+                shift.ExactStartTime = timeNow;
+                shift.ShiftType = ShiftType.OnGoing;
+                shiftRepository.Edit(shift);
+                commitData.SaveChanges();
+            }
+            else
+            {
+                return false;
+            }
+            IQueryable<Appointment> appointments = appointmentRepository.GetAllAppointmentForShift(ShiftId);
+
+
+            return true;
         }
     }
 }
