b4662c9ea047ef2427797ebc63ba3ffb7b9511e3
Amgad Montasser Adly <amgadnaser7@gmail.com>
2025-06-11 02:19:19 +0300

    Live Queue for Client

---
+++ Dorak.DataTransferObject/LiveQueueDTO/ClientLiveQueueDTO.cs
@@ -0,0 +1,22 @@
+﻿using Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dorak.DataTransferObject
+{
+    public class ClientLiveQueueDTO
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public int? CurrentQueuePosition { get; set; }
+        public TimeOnly? ArrivalTime { get; set; }
+        public DateOnly AppointmentDate { get; set; }
+        public ClientType Type { get; set; }
+        public QueueAppointmentStatus Status { get; set; }
+
+        public bool IsCurrentClient {  get; set; }
+    }
+}
